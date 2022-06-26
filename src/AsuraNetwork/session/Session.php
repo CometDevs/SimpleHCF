10389f342754dd2eac51f1ace723e5bc57323e6a
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-25 21:17:13 -0500

    update Session.php

--- src/AsuraNetwork/session/Session.php
+++ src/AsuraNetwork/session/Session.php
@@ -1,29 +1,48 @@
 <?php
 namespace AsuraNetwork\session;
 
+use AsuraNetwork\systems\factions\Faction;
 use pocketmine\player\Player;
 
 /**
  * Class Session
  * @package AsuraNetwork\session
  */
-class Session{
+class Session {
   
   /** @var Player $player */
   private Player $player;
+  
+  /** @var Faction $faction */
+  private Faction $faction;
   
   /**
    * Loader constructor.
    * @param Player $player
    */
-  public function __construct(Player $player){
+  public function __construct(Player $player) {
     $this->player = $player;
+    $this->loadUserData();
+  }
+  
+  /**
+   * @return Faction|null
+   */
+  public function getFaction(): ?Faction {
+    return $this->faction;
+  }
+  
+  /**
+   * @return void
+   */
+  public function loadUserData(): void {
+    
   }
   
   /**
    * @return Player
    */
-  public function getPlayer(): Player{
+  public function getPlayer(): Player {
     return $this->player;
   }
 }
