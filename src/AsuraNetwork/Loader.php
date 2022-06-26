57f52fff36427f8d0a9d12adbd10939f442f13a6
wXav <xavier04.martinez@gmail.com>
2022-06-26 00:58:30 -0500

    beautiful

--- src/AsuraNetwork/Loader.php
+++ src/AsuraNetwork/Loader.php
@@ -13,9 +13,11 @@ class Loader extends PluginBase{
 
     public function onEnable(): void{
         $this->saveConfig();
+
         $this->saveResource("abilities.yml");
         $this->saveResource("cooldowns.yml");
         $this->saveResource("faction.yml");
+
         FactionsFactory::getInstance()->init();
         SessionFactory::getInstance()->init();
         // Please don't use method static with main, use SingletonTrait
