7184295d66d209337710a3d83644373cc42b3538
ImAMadDev <30436794+ImAMadDev@users.noreply.github.com>
2022-06-26 02:05:05 -0500

    configs

--- src/AsuraNetwork/Loader.php
+++ src/AsuraNetwork/Loader.php
@@ -4,6 +4,8 @@ namespace AsuraNetwork;
 
 use AsuraNetwork\economy\EconomyFactory;
 use AsuraNetwork\factions\FactionsFactory;
+use AsuraNetwork\factions\listener\FactionListener;
+use AsuraNetwork\session\listener\SessionListener;
 use AsuraNetwork\session\SessionFactory;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
@@ -13,16 +15,22 @@ class Loader extends PluginBase{
 
     public function onEnable(): void{
         $this->saveConfig();
+        date_default_timezone_set($this->getConfig()->get("time-zone", "America/Chicago"));
+
         $this->saveResource("abilities.yml");
         $this->saveResource("cooldowns.yml");
         $this->saveResource("faction.yml");
+
         FactionsFactory::getInstance()->init();
         SessionFactory::getInstance()->init();
         // Please don't use method static with main, use SingletonTrait
         EconomyFactory::getInstance()->init();
+        $this->initListeners();
     }
 
     private function initListeners(): void{
-
+        foreach ([new SessionListener(), new FactionListener()] as $listener) {
+            $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+        }
     }
 }
