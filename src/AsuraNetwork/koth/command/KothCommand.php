9a2bbe775781f5b055c35178a109dd4af42919d9
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-27 13:55:24 -0500

    Base KothCommand.php

---
+++ src/AsuraNetwork/koth/command/KothCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace AsuraNetwork\koth\command;
+
+use AsuraNetwork\koth\command\subcommands\HelpSubCommand;
+use CortexPE\Commando\BaseCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+/**
+* Class KothCommand
+* @package AsuraNetwork\koth\command
+*/
+class KothCommand extends BaseCommand {
+
+/**
+* @return void
+*/
+protected function prepare(): void {
+$this->setUsage(TextFormat::GREEN . "/koth help");
+$this->registerSubCommand(new HelpSubCommand("help", "Koth commands."));
+}
+
+/**
+* @param CommandSender $sender
+* @param string $aliasUsed
+* @param array $args
+* @return void
+*/
+public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+  // Nothing??
+}
+}
