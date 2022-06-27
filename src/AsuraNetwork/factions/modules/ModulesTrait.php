6640ca5f3783fdaae78ef7b641c0abf900fae5b9
ImAMadDev <30436794+ImAMadDev@users.noreply.github.com>
2022-06-27 16:35:09 -0500

    Update ModulesTrait.php

--- src/AsuraNetwork/factions/modules/ModulesTrait.php
+++ src/AsuraNetwork/factions/modules/ModulesTrait.php
@@ -34,4 +34,64 @@ trait ModulesTrait{
         return intval($this->modules[self::KOTH_CAPPED_MODULE]->get());
     }
 
+    public function addKills(int $amount = 1): void{
+        $this->modules[self::KILLS_MODULE]->add($amount);
+    }
+
+    public function addBalance(int $amount): void{
+        $this->modules[self::BALANCE_MODULE]->add($amount);
+    }
+
+    public function addDTR(float $amount): void{
+        $this->modules[self::DTR_MODULE]->add($amount);
+    }
+
+    public function addPoints(int $amount): void{
+        $this->modules[self::POINTS_MODULE]->add($amount);
+    }
+
+    public function addKOTHCapped(int $amount): void{
+        $this->modules[self::KOTH_CAPPED_MODULE]->add($amount);
+    }
+
+    public function reduceKills(int $amount = 1): void{
+        $this->modules[self::KILLS_MODULE]->reduce($amount);
+    }
+
+    public function reduceBalance(int $amount): void{
+        $this->modules[self::BALANCE_MODULE]->reduce($amount);
+    }
+
+    public function reduceDTR(float $amount): void{
+        $this->modules[self::DTR_MODULE]->reduce($amount);
+    }
+
+    public function reducePoints(int $amount): void{
+        $this->modules[self::POINTS_MODULE]->reduce($amount);
+    }
+
+    public function reduceKOTHCapped(int $amount): void{
+        $this->modules[self::KOTH_CAPPED_MODULE]->reduce($amount);
+    }
+
+    public function setKills(int $amount = 1): void{
+        $this->modules[self::KILLS_MODULE]->set($amount);
+    }
+
+    public function setBalance(int $amount): void{
+        $this->modules[self::BALANCE_MODULE]->set($amount);
+    }
+
+    public function setDTR(float $amount): void{
+        $this->modules[self::DTR_MODULE]->set($amount);
+    }
+
+    public function setPoints(int $amount): void{
+        $this->modules[self::POINTS_MODULE]->set($amount);
+    }
+
+    public function setKOTHCapped(int $amount): void{
+        $this->modules[self::KOTH_CAPPED_MODULE]->set($amount);
+    }
+
 }
