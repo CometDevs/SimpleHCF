bb496fca0c6bd527374c4faeb41991ca05882325
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-27 13:59:33 -0500

    cleaned.

--- src/AsuraNetwork/koth/command/KothCommand.php
+++ src/AsuraNetwork/koth/command/KothCommand.php
@@ -2,7 +2,7 @@
 namespace AsuraNetwork\koth\command;
 
 use AsuraNetwork\koth\command\subcommands\HelpSubCommand;
-use CortexPE\Commando\BaseCommand;
+use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 
@@ -12,21 +12,21 @@ use pocketmine\utils\TextFormat;
 */
 class KothCommand extends BaseCommand {
 
-/**
-* @return void
-*/
-protected function prepare(): void {
-$this->setUsage(TextFormat::GREEN . "/koth help");
-$this->registerSubCommand(new HelpSubCommand("help", "Koth commands."));
-}
+  /**
+  * @return void
+  */
+  protected function prepare(): void {
+    $this->setUsage(TextFormat::GREEN . "/koth help");
+    $this->registerSubCommand(new HelpSubCommand("help", "Koth commands."));
+  }
 
-/**
-* @param CommandSender $sender
-* @param string $aliasUsed
-* @param array $args
-* @return void
-*/
-public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-  // Nothing??
-}
+  /**
+  * @param CommandSender $sender
+  * @param string $aliasUsed
+  * @param array $args
+  * @return void
+  */
+  public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+    // Nothing??
+  }
 }
